7b5923325f150ac7bc3cd1bec2fdaf5823464229
Prathika <prathika.sundaramurthy@ibm.com>
2023-06-21 00:05:44 +0530

    basic examples

---
+++ hello-world.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	// simple print statement
+	fmt.Println("hello-world")
+
+	// variable assigning
+	var name = "golang"
+	fmt.Println(name)
+
+	// declaring type of variable
+	var year int
+	year = 2023
+	fmt.Println(year)
+
+	// without declaring
+	version := "v 1.21.0"
+	fmt.Println(version)
+}
